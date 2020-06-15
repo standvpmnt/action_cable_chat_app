63d19eec829a430ffe4d8c04db6053660ffd6267
STan Dvpmnt <standvpmnt@gmail.com>
2020-06-15 09:11:38 +0530

    Setting up the basics

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  mount ActionCable.server, at: '/cable'
+  # config.action_cable.allowed_request_origins = [
+  #   'https://rails-tutorial-mhartl.c9users.io' ]
 end
